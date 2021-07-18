fc490d11c62922c8efbc888405940d2a634b2983
arinjayj1234 <80298655+arinjayj1234@users.noreply.github.com>
2021-07-18 15:22:54 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+color = "black";
+lwidth = 3;
+radius = 8;
+
+var width = screen.width;
+new_width = screen.width-70;
+new_height = screen.height-300;
+if (width<992) {
+    document.getElementById("myCanvas").width = new_width;
+    document.getElementById("myCanvas").height = new_height;
+    document.body.style.overflow = "hidden";
+}
+
+canvas.addEventListener(touchstart, "my_touchstart");
+
+function my_touchstart(e) {
+    console.log("my_touchstart")
+    color = document.getElementById("color").value;
+    lwidth = document.getElementById("lwidth").value;
+    radius = document.getElementById("radius").value;
+}
+
+canvas.addEventListener(touchmove, "my_touchmove");
+
+function my_touchmove(e) {
+    cpox = e.touches[0].clientX - canvas.offsetLeft;
+    cpoy = e.touches[0].clientY - canvas.offsetTop;
+    onsole.log("Current Position of X and Y Coordinate = ");
+    console.log("X = "+cpox+"Y = "+cpoy);
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth = lwidth;
+    ctx.arc(cpox, cpoy, radius, 0, 2*Math.PI);
+    ctx.stroke();
+}
+
+function cleararea() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
